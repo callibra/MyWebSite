5b3ef2449e4669f887e0a3132721ae9b1708670d
Ivan Gjorcev <gjorcev.ivan@gmail.com>
2024-09-11 20:22:38 +0200

    Update

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my React App!" />
+        <Header title="Welcome to my first React App!" />
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
